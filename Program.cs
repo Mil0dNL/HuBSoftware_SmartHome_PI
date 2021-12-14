d66cd5e26a0de21788d399989223c58ab92b2464
Milo Dorigo <489348@student.fontys.nl>
2021-12-14 10:54:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HuBSoftware_SmartHome_PI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string inp = Console.ReadLine();
+            Console.WriteLine($"You said {inp}!");
+        }
+    }
+}
